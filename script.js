7faeab0871bfb9862de07a376eec6a931e8b9a64
Roshan Chaudhari <98824802+roshanchaudhari7@users.noreply.github.com>
2024-07-21 16:02:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let butt_on = document.querySelector('input[type="button"][value="Select and Remove"]');
+let select = document.querySelector("#colorSelect");
+
+butt_on.addEventListener("click", deleted);
+
+function deleted() {
+	let selected = select.selected;
+    select.remove(selected);
+}
